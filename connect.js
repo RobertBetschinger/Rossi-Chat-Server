97d23be4769bd2159181696cb313e71b8d694e6b
unknown <robert-betschinger@outlook.de>
2021-04-26 14:27:58 +0200

    Wunderschön

--- connect.js
+++ connect.js
@@ -1,85 +1,106 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
 require("./models/user.model.js");
 require("./models/message.model.js");
 const User = mongoose.model("User");
 const Message = mongoose.model("Message");
 
-function connect(){
-    mongoose.connect("mongodb+srv://rossi-chat-server:"+ process.env.MONGO_ATLAS_CREDS +"@cluster0.clgcc.mongodb.net/Rossi-Chat-App?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-    if (!err) {console.log("MongoDB connection succeeded")}
-    else {return (err)}
-    })
-};
+function connect() {
+  mongoose.connect(
+    "mongodb+srv://rossi-chat-server:" +
+      process.env.MONGO_ATLAS_CREDS +
+      "@cluster0.clgcc.mongodb.net/Rossi-Chat-App?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+      if (!err) {
+        console.log("MongoDB connection succeeded");
+      } else {
+        return err;
+      }
+    }
+  );
+}
 
 // Disconnect der DB optional
 //function disconnect() {
 //    mongoose.connection.close()
 //};
 
-
-function addNewUser(userObject){
-    var user = new User(userObject);
-    user.save((err, doc) => {
-        if(!err){
-            console.log("User added to db");
-            return doc;
-        }         
-    });
-};
+function addNewUser(userObject) {
+  var user = new User(userObject);
+  user.save((err, doc) => {
+    if (!err) {
+      console.log("User added to db");
+      return doc;
+    }
+  });
+}
 
 function findUserById(userId) {
-    User.findOne({"userId":userId},function(err,user){
-        if (err) return handleError (err);
-        console.log('Entry found: %s %s %s', user.userId, user.number,
-        user.pseudonym);
-    })
-};
+  User.findOne({ userId: userId }, function (err, user) {
+    if (err) return handleError(err);
+    console.log(
+      "Entry found: %s %s %s",
+      user.userId,
+      user.number,
+      user.pseudonym
+    );
+  });
+}
 
- function findUserByNumber(number) {
-   const response =  User.findOne({"number":number},function(err,user){
-        if (err) return handleError (err);
-        console.log('Entry found: %s %s %s', user.userId, user.number,
-        user.spitzname)
-    }).then(
-        function(value){
-            console.log("Response Recieved")
-            console.log("das Ist der Value in Connect.js" + value)
-            return value
-        }).catch()
-            console.log("Hat in Connect nicht geklappt")
-
-        };
-
-
+async function findUserByNumber(number) {
+  const response = await User.findOne({ number: number }, function (err, user) {
+    if (err) return handleError(err);
+    console.log(
+      "Entry found: %s %s %s",
+      user.userId,
+      user.number,
+      user.spitzname
+    );
+  }).exec();
+}
 
 function addMessage(message) {
-    var message = new Message(message);
-    message.save((err, doc) => {
-        if(!err) {
-            console.log("Message added to db");
-            return doc;
-        }
-    });
-};
+  var message = new Message(message);
+  message.save((err, doc) => {
+    if (!err) {
+      console.log("Message added to db");
+      return doc;
+    }
+  });
+}
 
 function findMessage(messageId) {
-    Message.findOne({"messageId":messageId},function(err,message){
-        if (err) return handleError (err);
-        console.log('Entry found: %s %s %s', message.messageId, message.creatorId,
-        message.Message);
-    })
-};
+  Message.findOne({ messageId: messageId }, function (err, message) {
+    if (err) return handleError(err);
+    console.log(
+      "Entry found: %s %s %s",
+      message.messageId,
+      message.creatorId,
+      message.Message
+    );
+  });
+}
 
 function deleteMessage(messageId) {
-    Message.findOneAndDelete({"messageId":messageId},function(err,message){
-        if (err) return handleError (err);
-        console.log('Entry deleted: %s %s %s', message.messageId, message.creatorId,
-        message.Message);
-    })
+  Message.findOneAndDelete({ messageId: messageId }, function (err, message) {
+    if (err) return handleError(err);
+    console.log(
+      "Entry deleted: %s %s %s",
+      message.messageId,
+      message.creatorId,
+      message.Message
+    );
+  });
+}
+
+module.exports = {
+  connect,
+  addNewUser,
+  addMessage,
+  findUserById,
+  findUserByNumber,
+  findMessage,
+  deleteMessage,
 };
-
-
-module.exports = {connect, addNewUser, addMessage, findUserById, findUserByNumber, findMessage, deleteMessage};
-
