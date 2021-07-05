69b5062bfb77362a65fadfcbc5000947e0ca89b0
Robert Betschinger <robert-betschinger@outlook.de>
2021-07-05 12:10:15 +0200

    Substring 36

--- server.js
+++ server.js
@@ -863,14 +863,14 @@ var ID = function () {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
-  return Math.random().toString(36).substr(2, 9);
+  return Math.random().toString(36).substr(2, 36);
 };
 
 var PrivateID = function () {
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
-  return "_" + Math.random().toString(36).substr(2, 9);
+  return "_" + Math.random().toString(36).substr(2, 36);
 };
 
 //This Part has to be at the bottom of the Code
